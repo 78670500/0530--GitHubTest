6fd32af7ff06bae8999c12681b8b6dc8d8317466
78670500 <78670500@qq.com>
2016-05-30 09:49:12 +0800

    lll
    啦啦啦

---
+++ 0530练习-GitHubTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  0530练习-GitHubTest
+//
+//  Created by BEVER on 16/5/30.
+//  Copyright © 2016年 张凡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 0530练习-GitHubTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  0530练习-GitHubTest
+//
+//  Created by BEVER on 16/5/30.
+//  Copyright © 2016年 张凡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
